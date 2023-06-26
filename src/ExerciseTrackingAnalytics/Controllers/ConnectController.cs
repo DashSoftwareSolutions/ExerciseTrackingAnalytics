616c742e974f1ef4a66f3def7c916b2b87adb4dc
Geoffrey Roberts <groberts314@yahoo.com>
2023-06-25 17:37:41 -0700

    feat: WIP on home page and Connect to Strava workflow

---
+++ src/ExerciseTrackingAnalytics/Controllers/ConnectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciseTrackingAnalytics.Controllers
+{
+    [Route("Connect")]
+    public class ConnectController : Controller
+    {
+        private readonly ILogger<ConnectController> _logger;
+
+        public ConnectController(ILogger<ConnectController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public Task<ActionResult> Connect()
+        {
+            return Task.FromResult(View() as ActionResult);
+        }
+    }
+}

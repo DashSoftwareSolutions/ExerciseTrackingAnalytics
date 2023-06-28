0ecadc77f1d6336cf541fd416ea48b2adc77d1cd
Geoffrey Roberts <groberts314@yahoo.com>
2023-06-27 20:03:05 -0700

    feat: add navigation properties for Application User
    Hoping this will help us fetch the Tokens and whatever else we need

--- src/ExerciseTrackingAnalytics/Models/ApplicationUser.cs
+++ src/ExerciseTrackingAnalytics/Models/ApplicationUser.cs
@@ -18,5 +18,13 @@ namespace ExerciseTrackingAnalytics.Models
         public DateTime? EmailConfirmedDate { get; set; }
 
         public DateTime? PhoneConfirmedDate { get; set; }
+
+        public virtual ICollection<IdentityUserClaim<Guid>>? Claims { get; set; }
+
+        public virtual ICollection<IdentityUserLogin<Guid>>? Logins { get; set; }
+
+        public virtual ICollection<IdentityUserToken<Guid>>? Tokens { get; set; }
+
+        public virtual ICollection<IdentityUserRole<Guid>>? UserRoles { get; set; }
     }
 }

a65345f141c879c2e7150441ae5b02892e55810a
Geoffrey Roberts <groberts314@yahoo.com>
2023-12-21 10:11:19 -0800

    feat: add an extension method to stringify time spans as hh:mm:ss
    Want to be able to show an "improper" number of hours (more than 24), versus the default behavior, which includes a days component, then a decimal point, then hh:mm:ss for only a fractional day.

---
+++ src/ExerciseTrackingAnalytics/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace ExerciseTrackingAnalytics.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        public static string ToHoursMinutesSecondsString(this TimeSpan timeSpan)
+        {
+            var totalHours = (int)Math.Floor(timeSpan.TotalHours);
+
+            return $"{totalHours.ToString("D2")}:{timeSpan.Minutes.ToString("D2")}:{timeSpan.Seconds.ToString("D2")}";
+        }
+    }
+}
---
+++ test/ExerciseTrackingAnalytics.Tests/Extensions/TimeSpanExtensionsFixture.cs
@@ -0,0 +1,15 @@
+﻿using ExerciseTrackingAnalytics.Extensions;
+
+namespace ExerciseTrackingAnalytics.Tests.Extensions
+{
+    public class TimeSpanExtensionsFixture
+    {
+        [Theory]
+        [InlineData("3:45:23", "03:45:23")]
+        [InlineData("1.01:23:52", "25:23:52")]
+        public void ToHoursMinutesSecondsString_Works(string parseableTimeSpan, string expectedOutput)
+        {
+            Assert.Equal(expectedOutput, TimeSpan.Parse(parseableTimeSpan).ToHoursMinutesSecondsString());
+        }
+    }
+}

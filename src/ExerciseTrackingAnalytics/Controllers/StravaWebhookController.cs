c7741dd4a0713255c4df955c74736405e51448a8
Geoffrey Roberts <groberts314@yahoo.com>
2023-12-30 09:51:43 -0800

    feat: stubbed out a Strava Webhook

---
+++ src/ExerciseTrackingAnalytics/Controllers/StravaWebhookController.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using ExerciseTrackingAnalytics.ViewModels;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace ExerciseTrackingAnalytics.Controllers
+{
+    [Route("api/strava-webhook")]
+    [ApiController]
+    public class StravaWebhookController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public StravaWebhookController(ILogger<StravaWebhookController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var queryStringParams = HttpContext.Request.Query;
+            var formattedQueryParams = string.Join("; ", queryStringParams.Select(qsKvp => string.Format("{0}={1}", qsKvp.Key, qsKvp.Value)));
+
+            _logger.LogDebug("Strava Webhook's GET endpoint was called the the following parameters: {queryStringParams}", formattedQueryParams);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] StravaWebhookEventViewModel webhookEvent)
+        {
+            if (webhookEvent == null)
+            {
+                return Problem(
+                    "POST body did not conform to the expected format and could not be properly parsed",
+                    instance: HttpContext.Request.GetDisplayUrl(),
+                    statusCode: StatusCodes.Status400BadRequest,
+                    title: "Invalid POST body",
+                    type: "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400");
+            }
+
+            _logger.LogDebug("Received Strava Webhook event: {webhookEvent}", JsonSerializer.Serialize(webhookEvent));
+
+            return Ok();
+        }
+    }
+}
---
+++ src/ExerciseTrackingAnalytics/ViewModels/StravaWebhookEventViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExerciseTrackingAnalytics.ViewModels
+{
+    public class StravaWebhookEventViewModel
+    {
+        [JsonPropertyName("aspect_type")]
+        public string AspectType { get; set; } = string.Empty;
+
+        [JsonPropertyName("event_time")]
+        public long EventTime { get; set; }
+
+        [JsonPropertyName("object_id")]
+        public long ObjectId { get; set; }
+
+        [JsonPropertyName("object_type")]
+        public string ObjectType { get; set; } = string.Empty;
+
+        [JsonPropertyName("owner_id")]
+        public long OwnerId { get; set; }
+
+        [JsonPropertyName("subscription_id")]
+        public long SubscriptionId { get; set; }
+
+        [JsonPropertyName("updates")]
+        public Dictionary<string, object> Updates { get; set; } = new();
+    }
+}
